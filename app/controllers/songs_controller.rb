15add913cb68f0cd38e217ad0528858122dea9d7
Chris Franklin <cf73021@gmail.com>
2018-11-13 20:16:54 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+  post '/songs' do
+    genre_ids = params["genres"] || []
+    @song = Song.new(name: params[:song_name])
+      genre_ids.each do |genre_id|
+      genre = Genre.find_by(id: genre_id)
+      if genre
+        @song.genres << genre
+      end
+    end
+	    redirect to "songs/#{@song.slug}"
+  end
+
+end
