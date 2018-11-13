454a1d6bd41549cef1c4015f9092d2f9ae85d3f5
Chris Franklin <cf73021@gmail.com>
2018-11-13 20:39:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,27 +19,44 @@ class SongsController < ApplicationController
     song_name = params["song"]["name"]
     artist_name = params["artist"]["name"]
     genre_ids = params["genres"] || []
-
-    if song_name
-      @song = Song.new(name: params[:song_name])
+     if song_name
+      @song = Song.new(name: song_name)
       if artist_name != ""
         @song.artist = Artist.find_or_create_by(name: artist_name)
       end
     else
       @error_message = "You must enter a song name!"
       @genres = Genre.all
-      erb :'/songs/new'
+      erb :new
     end
-
-    genre_ids.each do |genre_id|
+     genre_ids.each do |genre_id|
       genre = Genre.find_by(id: genre_id)
       if genre
         @song.genres << genre
       end
     end
-
-    @song.save
-    redirect to "/songs/#{@song.slug}"
+     @song.save
+    flash[:notice] = "Successfully created song."
+     redirect to "/songs/#{@song.slug}"
+  end
+   patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    song_name = params["song"]["name"]
+    artist_name = params["artist"]["name"]
+     if song_name
+      @song.name = song_name
+      artist = Artist.find_or_create_by(name: params["artist"]["name"])
+      if artist
+        @song.artist = artist
+      end
+       @song.genres = []
+      params["genres"].each do |genre_id|
+        @song.genres << Genre.find(genre_id)
+      end
+    end
+     @song.save
+    flash[:notice] = "Successfully updated song."
+    redirect to :"/songs/#{@song.slug}"
   end
 
 end
